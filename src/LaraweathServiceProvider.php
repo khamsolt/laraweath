dfedd22d72e14ead9d663595ac2111452932c085
khamsolt <khamidov.che@gmail.com>
2020-03-28 14:39:52 +0300

    0.1.0 Initial Release.

---
+++ src/LaraweathServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Khamsolt\Laraweath;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraweathServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
