0c671f28be9eac25c0195a4b782a973683247e6e
khamsolt <khamidov.che@gmail.com>
2020-03-29 23:45:40 +0300

    0.1.1 Configuration files have been added.

---
+++ config/laraweath.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    /**
+     * Your API access key
+     */
+    'token' => 'YOUR_API_TOKEN',
+
+    /**
+     * This is default city
+     */
+    'defaultCity' => 'Grozny',
+
+    /**
+     * This parameter to pass one of the unit identifiers ot the API:
+     * m - for Metric
+     * s - for Scientific
+     * f - for Fahrenheit
+     *
+     * @see https://weatherstack.com/documentation#units_parameter
+     */
+    'units' => 'm',
+
+    /**
+     * this parameter to specify your preferred API response language using its ISO-code.
+     *
+     * @see https://weatherstack.com/documentation#language_parameter
+     */
+    'language' => 'ru'
+];
--- src/LaraweathServiceProvider.php
+++ src/LaraweathServiceProvider.php
@@ -10,11 +10,13 @@ class LaraweathServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-
+        $this->publishes([
+            __DIR__ . '/../config/laraweath.php' => config_path('laraweath.php'),
+        ]);
     }
 
     public function register()
     {
-
+        $this->mergeConfigFrom(__DIR__ . '/../config/laraweath.php', 'laraweath');
     }
 }
